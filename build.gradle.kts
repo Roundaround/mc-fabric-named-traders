9282124b4e7fc992c62973c94bbfb7d755ae6b5a
Evan Steinkerchner <esteinkerchner@gmail.com>
2024-12-07 15:46:04 -0500

    Update to 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
-  id("roundalib") version "0.3.6"
+  id("fabric-loom") version "1.9-SNAPSHOT"
+  id("com.gradleup.shadow") version "9.0.0-beta2"
+  id("roundalib") version "0.9.0-SNAPSHOT"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 pluginManagement {
   repositories {
+    mavenCentral()
     gradlePluginPortal()
     maven("https://maven.fabricmc.net/")
-    maven("https://maven.rnda.dev/releases/")
+    maven("https://maven.rnda.dev/snapshots/")
   }
 }
